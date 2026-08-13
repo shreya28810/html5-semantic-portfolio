fc2a4ccd2c87a9b6b077f264ea49d2c15cf02dd5
shreya28810 <shettyvijaya081@gmail.com>
2026-08-13 20:17:20 +0530

    Week 2: Advanced CSS3 and Responsive Design

---
+++ script.js
@@ -0,0 +1,17 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+
+        themeToggle.textContent = "☀️ Light Mode";
+
+    } else {
+
+        themeToggle.textContent = "🌙 Dark Mode";
+
+    }
+
+});
